395385bb5e859b16c2a2d215b75876a9556ddcb1
NaZe0320 <rwt1226@naver.com>
2024-01-15 20:06:20 +0900

    [add] add fragment (4)

---
+++ app/src/main/java/com/umc/coumo/presentation/fragment/CouponFragment.kt
@@ -0,0 +1,8 @@
+package com.umc.coumo.presentation.fragment
+
+import com.umc.coumo.R
+import com.umc.coumo.databinding.FragmentCouponBinding
+import com.umc.coumo.utils.binding.BindingFragment
+
+class CouponFragment: BindingFragment<FragmentCouponBinding>(R.layout.fragment_coupon) {
+}
