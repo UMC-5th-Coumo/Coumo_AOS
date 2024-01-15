b1ea1b6a943e4013878825c867980062ca77554c
NaZe0320 <rwt1226@naver.com>
2024-01-16 07:27:10 +0900

    [add] 다이얼 연결

--- app/src/main/java/com/umc/coumo/presentation/fragment/AccountFragment.kt
+++ app/src/main/java/com/umc/coumo/presentation/fragment/AccountFragment.kt
@@ -1,8 +1,23 @@
 package com.umc.coumo.presentation.fragment
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
 import com.umc.coumo.R
 import com.umc.coumo.databinding.FragmentAccountBinding
 import com.umc.coumo.utils.binding.BindingFragment
 
 class AccountFragment: BindingFragment<FragmentAccountBinding>(R.layout.fragment_account) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.sectionServiceCenter.setOnClickListener {
+            val dialIntent = Intent(Intent.ACTION_DIAL).apply {
+                data = Uri.parse("tel:" + "1577-9999")
+            }
+            startActivity(dialIntent)
+        }
+    }
 }
