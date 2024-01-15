d1f1b17865889f1c30a8f43393348bd7c928ff74
NaZe0320 <rwt1226@naver.com>
2024-01-15 20:00:10 +0900

    [add] add HomeFragment and bottom navi background

--- app/src/main/java/com/umc/coumo/presentation/activity/MainActivity.kt
+++ app/src/main/java/com/umc/coumo/presentation/activity/MainActivity.kt
@@ -3,10 +3,13 @@ package com.umc.coumo.presentation.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.umc.coumo.R
+import com.umc.coumo.databinding.ActivityMainBinding
+import com.umc.coumo.utils.binding.BindingActivity
+import dagger.hilt.android.AndroidEntryPoint
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
     }
 }
---
+++ app/src/main/java/com/umc/coumo/presentation/fragment/HomeFragment.kt
@@ -0,0 +1,9 @@
+package com.umc.coumo.presentation.fragment
+
+import com.umc.coumo.R
+import com.umc.coumo.databinding.FragmentHomeBinding
+import com.umc.coumo.utils.binding.BindingFragment
+
+class HomeFragment: BindingFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+
+}
