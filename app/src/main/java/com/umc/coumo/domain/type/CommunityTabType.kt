5306b7083e02a76945619408662b3e7a32c11c1e
NaZe0320 <rwt1226@naver.com>
2024-01-21 01:08:02 +0900

    [ui/#11]: type에 페이지 타이틀 데이터 추가

--- app/src/main/java/com/umc/coumo/domain/type/CommunityTabType.kt
+++ app/src/main/java/com/umc/coumo/domain/type/CommunityTabType.kt
@@ -1,8 +1,8 @@
 package com.umc.coumo.domain.type
 
-enum class CommunityTabType() {
-    ALL,
-    NEW_MENU,
-    NO_SHOW,
-    EVENT
+enum class CommunityTabType(title: String?) {
+    ALL(null),
+    NEW_MENU("우리동네 신메뉴들을 찾아봤어요"),
+    NO_SHOW("우리동네 노쇼빈자리 SALE을 찾아봤어요"),
+    EVENT("우리동네 이벤트들을 찾아봤어요")
 }
