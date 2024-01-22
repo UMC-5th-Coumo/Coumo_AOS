5ca42fbe6ebe9af51ab1f34103955cb0cd6853ce
NaZe0320 <rwt1226@naver.com>
2024-01-22 16:08:51 +0900

    [ui/#12]: FAB 임시 구현 및 쿠폰 리스트 화면, QR 화면 분리

--- app/src/main/java/com/umc/coumo/presentation/fragment/CouponQRFragment.kt
+++ app/src/main/java/com/umc/coumo/presentation/fragment/CouponQRFragment.kt
@@ -4,9 +4,11 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.umc.coumo.R
 import com.umc.coumo.databinding.FragmentCouponBinding
+import com.umc.coumo.databinding.FragmentCouponQrBinding
 import com.umc.coumo.domain.viewmodel.CommunityViewModel
 import com.umc.coumo.domain.viewmodel.CouponViewModel
 import com.umc.coumo.presentation.adapter.CouponAdapter
@@ -14,14 +16,20 @@ import com.umc.coumo.utils.binding.BindingFragment
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class CouponFragment: BindingFragment<FragmentCouponBinding>(R.layout.fragment_coupon) {
+class CouponQRFragment: BindingFragment<FragmentCouponQrBinding>(R.layout.fragment_coupon_qr) {
 
     private val viewModel: CouponViewModel by activityViewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.viewModel = viewModel
+        binding.lifecycleOwner = viewLifecycleOwner
+
     }
 
-
+    override fun onBackPressed() {
+        super.onBackPressed()
+        findNavController().popBackStack()
+    }
 }
