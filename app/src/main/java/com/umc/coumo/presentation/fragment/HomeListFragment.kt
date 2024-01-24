17a493d1ee1aecd7efb4bdc0fc1ce79c8366e1da
NaZe0320 <rwt1226@naver.com>
2024-01-25 06:51:28 +0900

    [ui/#10]: 메인 홈 가게 리스트 화면 ui 구현

--- app/src/main/java/com/umc/coumo/presentation/fragment/HomeListFragment.kt
+++ app/src/main/java/com/umc/coumo/presentation/fragment/HomeListFragment.kt
@@ -2,9 +2,13 @@ package com.umc.coumo.presentation.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.umc.coumo.R
 import com.umc.coumo.databinding.FragmentHomeListBinding
+import com.umc.coumo.domain.model.StoreCouponCountModel
+import com.umc.coumo.presentation.adapter.StoreCouponCountAdapter
 import com.umc.coumo.utils.binding.BindingFragmentNoneBackPress
 
 class HomeListFragment: BindingFragmentNoneBackPress<FragmentHomeListBinding>(R.layout.fragment_home_list) {
@@ -12,8 +16,39 @@ class HomeListFragment: BindingFragmentNoneBackPress<FragmentHomeListBinding>(R.
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.btnTest.setOnClickListener {
-            findNavController().navigate(R.id.action_homeListFragment_to_homeDetailFragment)
+        binding.lifecycleOwner = viewLifecycleOwner
+
+        val storeCouponAdapter = StoreCouponCountAdapter()
+
+        binding.rvStore.apply {
+            adapter = storeCouponAdapter
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         }
+
+        storeCouponAdapter.setOnItemClickListener(object : StoreCouponCountAdapter.OnItemClickListener {
+            override fun onItemClick(id: Int) {
+                val bundle = bundleOf("id" to id)
+                findNavController().navigate(R.id.action_homeListFragment_to_homeDetailFragment, bundle)
+            }
+        })
+
+        //테스트 코드
+        val list = listOf<StoreCouponCountModel>(
+            StoreCouponCountModel(1, null,"앙떼띠 로스터리(강남점)", 2),
+            StoreCouponCountModel(2, null,"앙떼띠 로스터리(강남점)", 5),
+            StoreCouponCountModel(3, null,"앙떼띠 로스터리(강남점)", 4),
+            StoreCouponCountModel(4, null,"앙떼띠 로스터리(강남점)", 3),
+            StoreCouponCountModel(5, null,"앙떼띠 로스터리(강남점)", 10),
+            StoreCouponCountModel(6, null,"앙떼띠 로스터리(강남점)", 0),
+            StoreCouponCountModel(11, null,"앙떼띠 로스터리(강남점)", 2),
+            StoreCouponCountModel(21, null,"앙떼띠 로스터리(강남점)", 5),
+            StoreCouponCountModel(31, null,"앙떼띠 로스터리(강남점)", 4),
+            StoreCouponCountModel(41, null,"앙떼띠 로스터리(강남점)", 3),
+            StoreCouponCountModel(51, null,"앙떼띠 로스터리(강남점)", 10),
+            StoreCouponCountModel(61, null,"앙떼띠 로스터리(강남점)", 0),
+        )
+
+        storeCouponAdapter.submitList(list)
+
     }
 }
