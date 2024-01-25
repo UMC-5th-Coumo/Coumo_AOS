06287e23971e021b64558d4af52f10cf58307772
psw9428 <psw9428@naver.com>
2024-01-25 15:12:21 +0900

    [ui/#20]: 메인 로그인 화면 및 회원가입 프론트 일부 구현

---
+++ app/src/main/java/com/umc/coumo/presentation/activity/LoginActivity.kt
@@ -0,0 +1,15 @@
+package com.umc.coumo.presentation.activity
+
+import android.os.Bundle
+import com.umc.coumo.R
+import com.umc.coumo.databinding.ActivityLoginBinding
+import com.umc.coumo.utils.binding.BindingActivity
+
+class LoginActivity : BindingActivity<ActivityLoginBinding>(R.layout.activity_login) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+
+
+    }
+}
