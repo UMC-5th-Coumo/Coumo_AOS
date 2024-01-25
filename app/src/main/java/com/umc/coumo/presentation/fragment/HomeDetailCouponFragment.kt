eee84144e50333ef0ddd0fc44b8c5eb65b868dbb
NaZe0320 <rwt1226@naver.com>
2024-01-25 10:00:54 +0900

    [ui/#10]: 내 쿠폰 보기 탭 ui 일부 구현

---
+++ app/src/main/java/com/umc/coumo/presentation/fragment/HomeDetailCouponFragment.kt
@@ -0,0 +1,9 @@
+package com.umc.coumo.presentation.fragment
+
+import com.umc.coumo.R
+import com.umc.coumo.databinding.FragmentHomeDetailCouponBinding
+import com.umc.coumo.utils.binding.BindingFragmentNoneBackPress
+
+class HomeDetailCouponFragment: BindingFragmentNoneBackPress<FragmentHomeDetailCouponBinding>(R.layout.fragment_home_detail_coupon) {
+
+}
