53de8e0f54230c1080764e4b415d4bed37d357c6
NaZe0320 <rwt1226@naver.com>
2024-01-25 09:37:09 +0900

    [ui/#10]: 매장 정보 탭 ui 일부 구현

---
+++ app/src/main/java/com/umc/coumo/presentation/fragment/HomeDetailInfoFragment.kt
@@ -0,0 +1,9 @@
+package com.umc.coumo.presentation.fragment
+
+import com.umc.coumo.R
+import com.umc.coumo.databinding.FragmentHomeDetailInfoBinding
+import com.umc.coumo.utils.binding.BindingFragmentNoneBackPress
+
+class HomeDetailInfoFragment: BindingFragmentNoneBackPress<FragmentHomeDetailInfoBinding>(R.layout.fragment_home_detail_info) {
+
+}
