2b50b1f15c1a1a5014478f6ae96090aca7dde9d2
NaZe0320 <rwt1226@naver.com>
2024-01-29 07:29:47 +0900

    [ui/#10]: menu Adapter 적용

---
+++ app/src/main/java/com/umc/coumo/presentation/adapter/MenuAdapter.kt
@@ -0,0 +1,87 @@
+package com.umc.coumo.presentation.adapter
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.umc.coumo.databinding.ItemMenuBinding
+import com.umc.coumo.databinding.ItemMenuDetailBinding
+import com.umc.coumo.domain.model.MenuModel
+import com.umc.coumo.utils.ItemDiffCallback
+
+class MenuAdapter(
+    private val type: Int
+): ListAdapter<MenuModel, RecyclerView.ViewHolder>(
+    ItemDiffCallback<MenuModel>(
+        onContentsTheSame = {old, new -> old == new},
+        onItemsTheSame = {old, new -> old.id == new.id}
+    )
+) {
+companion object {
+    const val MENU_ITEM = 0
+    const val MENU_DETAIL_ITEM = 1
+}
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return when (type) {
+            MENU_ITEM -> {
+                MenuViewHolder(ItemMenuBinding.inflate(inflater, parent,false))
+            }
+            MENU_DETAIL_ITEM -> {
+                MenuDetailViewHolder(ItemMenuDetailBinding.inflate(inflater, parent,false))
+            }
+            else -> throw IllegalArgumentException("Invalid viewType")
+        }
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val item = getItem(position)
+
+        when (holder) {
+            is MenuViewHolder -> {
+                holder.bind(item)
+            }
+            is MenuDetailViewHolder -> {
+                holder.bind(item)
+            }
+        }
+    }
+
+
+
+    inner class MenuViewHolder(
+        private val binding: ItemMenuBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: MenuModel) {
+            binding.item = item
+
+            itemView.setOnClickListener {
+                listener?.onItemClick(item.id)
+            }
+        }
+    }
+
+    inner class MenuDetailViewHolder(
+        private val binding: ItemMenuDetailBinding
+    ): RecyclerView.ViewHolder(binding.root) {
+        fun bind(item: MenuModel) {
+            binding.item = item
+
+            itemView.setOnClickListener {
+                listener?.onItemClick(item.id)
+            }
+        }
+    }
+
+    interface OnItemClickListener {
+        fun onItemClick(id:Int)
+    }
+
+    private var listener: OnItemClickListener? = null
+
+    fun setOnItemClickListener(listener: OnItemClickListener) {
+        this.listener = listener
+    }
+}
--- app/src/main/java/com/umc/coumo/presentation/fragment/HomeMenuFragment.kt
+++ app/src/main/java/com/umc/coumo/presentation/fragment/HomeMenuFragment.kt
@@ -2,14 +2,40 @@ package com.umc.coumo.presentation.fragment
 
 import android.os.Bundle
 import android.view.View
+import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.lifecycleScope
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.umc.coumo.R
 import com.umc.coumo.databinding.FragmentHomeMenuBinding
+import com.umc.coumo.domain.viewmodel.HomeViewModel
+import com.umc.coumo.presentation.adapter.MenuAdapter
 import com.umc.coumo.utils.binding.BindingFragmentNoneBackPress
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
 class HomeMenuFragment: BindingFragmentNoneBackPress<FragmentHomeMenuBinding>(R.layout.fragment_home_menu) {
+
+    private val viewModel : HomeViewModel by activityViewModels ()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        setRecyclerView()
+
+    }
+
+    private fun setRecyclerView() {
+        val menuAdapter = MenuAdapter(1)
+
+        binding.rvMenu.apply {
+            adapter = menuAdapter
+            layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
+        }
+
+        viewModel.menuList.observe(viewLifecycleOwner) {
+            viewLifecycleOwner.lifecycleScope.launch (Dispatchers.Main) {
+                menuAdapter.submitList(it)
+            }
+        }
     }
 }
