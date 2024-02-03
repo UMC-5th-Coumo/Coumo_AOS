a699afb39e2358ad5446b85fcb1fab29fab135a5
NaZe0320 <rwt1226@naver.com>
2024-02-03 09:57:22 +0900

    [feat/#34]: Info Item과 Page api model 구현

---
+++ app/src/main/java/com/umc/coumo/utils/ApiResponse.kt
@@ -0,0 +1,12 @@
+package com.umc.coumo.utils
+
+import com.umc.coumo.domain.model.Status
+
+
+data class ApiResponse<out T>(val status: Status, val data: T?, val throwable: Throwable?) {
+    companion object {
+        fun <T> success(data: T): ApiResponse<T> = ApiResponse(Status.SUCCESS, data, null)
+        fun <T> error(throwable: Throwable): ApiResponse<T> = ApiResponse(Status.ERROR, null, throwable)
+        fun <T> loading(data: T? = null): ApiResponse<T> = ApiResponse(Status.LOADING, data, null)
+    }
+}
