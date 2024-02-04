a22017cd3c2fb717f50cf8751b841b09b564a615
NaZe0320 <rwt1226@naver.com>
2024-02-04 11:25:27 +0900

    [feat/#34]: 상세 화면 지도 추가

--- app/src/main/java/com/umc/coumo/presentation/fragment/HomeDetailInfoFragment.kt
+++ app/src/main/java/com/umc/coumo/presentation/fragment/HomeDetailInfoFragment.kt
@@ -6,6 +6,12 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.umc.coumo.R
 import com.umc.coumo.databinding.FragmentHomeDetailInfoBinding
 import com.umc.coumo.domain.viewmodel.HomeViewModel
@@ -15,9 +21,13 @@ import com.umc.coumo.utils.binding.BindingFragmentNoneBackPress
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
-class HomeDetailInfoFragment: BindingFragmentNoneBackPress<FragmentHomeDetailInfoBinding>(R.layout.fragment_home_detail_info) {
+class HomeDetailInfoFragment: BindingFragmentNoneBackPress<FragmentHomeDetailInfoBinding>(R.layout.fragment_home_detail_info), OnMapReadyCallback {
 
     private val viewModel : HomeViewModel by activityViewModels ()
+    private lateinit var map: GoogleMap
+
+    private var location = LatLng(0.0,0.0)
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -42,7 +52,21 @@ class HomeDetailInfoFragment: BindingFragmentNoneBackPress<FragmentHomeDetailInf
         viewModel.storeData.observe(viewLifecycleOwner) {
             viewLifecycleOwner.lifecycleScope.launch (Dispatchers.Main) {
                 menuAdapter.submitList(it.menuList)
+                location = LatLng(it.latitude, it.longitude)
+                setMap()
             }
         }
+    }
+
+    private fun setMap() {
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+        map = p0
+
+        map.addMarker(MarkerOptions().position(location))
+        map.moveCamera(CameraUpdateFactory.newLatLngZoom(location,15f))
     }
 }
