eefdb0488b58119e90847cf7431e2bb2f56f473c
NaZe0320 <rwt1226@naver.com>
2024-02-07 21:21:02 +0900

    [feat/#38]: 가게 정보 Model 구현

---
+++ app/src/main/java/com/umc/coumo/data/remote/model/response/ResponseMenuModel.kt
@@ -0,0 +1,8 @@
+package com.umc.coumo.data.remote.model.response
+
+data class ResponseMenuModel(
+    val name: String,
+    val description: String,
+    val image: String,
+    val isNew: Boolean
+)
