e1c1d22037b389381f734de33722760393a6cad7
psw9428 <psw9428@naver.com>
2024-02-15 19:48:20 +0900

    [api/#58] : 실제 존재하는 계정으로 로그인이 가능하도록 연결

--- app/src/main/java/com/umc/coumo/domain/viewmodel/LoginViewModel.kt
+++ app/src/main/java/com/umc/coumo/domain/viewmodel/LoginViewModel.kt
@@ -23,26 +23,12 @@ class LoginViewModel @Inject constructor(
     private val _isNotValidateAccount = MutableLiveData(false)
     val isNotValidateAccount: LiveData<Boolean> get() = _isNotValidateAccount
 
-    private val _loginResult = MutableLiveData<Boolean>()
+    private val _loginResult = MutableLiveData<Boolean>(false)
     val loginResult: LiveData<Boolean> get() = _loginResult
 
     private val _afterPressLoginBtn = MutableLiveData<Boolean>(false)
     val afterPressLoginBtn: LiveData<Boolean> get() = _afterPressLoginBtn
 
-    //TODO(테스트 코드)
-    fun postJoin(
-        loginId: String,
-        password: String,
-        name: String,
-        birthday: String,
-        gender: String,
-        email: String,
-        phone: String
-    ) {
-        viewModelScope.launch {
-            repository.postJoin(loginId, password, name, birthday, gender, email, phone)
-        }
-    }
 
     //TODO(테스트 코드)
     fun postLogin(loginId: String, password: String) {
@@ -52,6 +38,6 @@ class LoginViewModel @Inject constructor(
     }
 
     fun trueAfterPressLoginBtn() { _afterPressLoginBtn.value = true }
-
+    fun setLoginResult(bool : Boolean) { _loginResult.value = bool }
     fun setIsNotValidateAccount(bool: Boolean) { _isNotValidateAccount.value = bool }
 }
